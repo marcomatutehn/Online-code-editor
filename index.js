d193f8e0bc287f78e5c6cf1f6b97ec515099b245
MarcoPC <marcomatute@unah.hn>
2018-10-03 22:17:30 -0600

    First try to upload to heroku

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var express = require("express"); //Importar el framework express
-        var app = express(); //Inicializar la aplicacion con express
-        app.use(express.static("public")); //Publicar un directorio de archivos estaticos
-        app.listen(3000);//Levantar el servidor y escuchar en el puerto indicado
+
+var app = express(); //Inicializar la aplicacion con express
+
+const PORT = process.env.PORT ||3000;
+
+
+app.use(express.static("public")); //Publicar un directorio de archivos estaticos
+app.listen(PORT);//Levantar el servidor y escuchar en el puerto indicado
